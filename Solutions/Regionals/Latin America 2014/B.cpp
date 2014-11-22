aa5dd0ad1088e32fe02a36a32a7aec854213cf54
jariasf <jariasf03@gmail.com>
2014-11-21 21:55:45 -0500

    Latin America 2014 - Problem B Improved

--- Solutions/Regionals/Latin America 2014/B.cpp
+++ Solutions/Regionals/Latin America 2014/B.cpp
@@ -7,27 +7,31 @@
 #include <stdio.h>
 #include <algorithm>
 #include <cstring>
+#include <vector>
 
 using namespace std;
 #define MAX 5005
 char s[ MAX ];
 
 int main(){
-    int A , B, len , b , w;
+    int A , B, len , b;
     long long ans;
     while( scanf("%d %d" , &A , &B ) != EOF ){
         scanf("%s" , s );
         len = strlen( s );
-        ans = 0;
-        for( int i = len - 1 ; i >= 0 ; --i ){
+        ans = b = 0;
+        vector<int> w;
+
+        for( int i = 0 ; i < len ; ++i ){
+            if( s[i] == 'W' )
+                w.push_back(i);
+            else
+                b++;
+        }
+
+        for( int i = len - 1 , k = 0 ; i >= b ; --i ){
             if( s[ i ] == 'B' ){
-                int index = -1;
-                for( int j = 0 ; j < i ; ++j ){
-                    if( s[j] == 'W' ){
-                        index = j;
-                        break;
-                    }
-                }
+                int index = w[k++];
                 if( index != -1 ){
                     long long dif = ( A - B ) * (long long)( i - index );
                     if( dif > A ){
