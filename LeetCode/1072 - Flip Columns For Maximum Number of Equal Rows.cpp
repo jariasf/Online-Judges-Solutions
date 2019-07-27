b93826dec9f8fc2857e5bc5d4aeeb7ec3eeba302
jariasf <jariasf03@gmail.com>
2019-07-27 00:13:56 -0500

    Improved LeetCode 1072 Solution

--- LeetCode/1072 - Flip Columns For Maximum Number of Equal Rows.cpp
+++ LeetCode/1072 - Flip Columns For Maximum Number of Equal Rows.cpp
@@ -6,6 +6,7 @@
  ***Autor: Jhosimar George Arias Figueroa
  *******************************************/
 
+//Using map
 class Solution {
 public:
     int maxEqualRowsAfterFlips(vector<vector<int>>& matrix) {
@@ -16,6 +17,52 @@ public:
                 matrix[i][j] = !matrix[i][j];
             }
             hash[ matrix[i] ]++;
+        }
+
+        int result = 0;
+        for( auto it: hash ){
+            result = max( result, it.second);
+        }
+        return result;
+    }   
+};
+
+
+//Using 2 strings and unordered_map
+class Solution {
+public:
+    int maxEqualRowsAfterFlips(vector<vector<int>>& matrix) {
+        unordered_map<string, int> hash;
+        for( int i = 0 ; i < matrix.size(); ++i ){
+            string current = "", flip = "";
+            for( int j = 0 ; j < matrix[i].size(); ++j ){
+                current += (char)(matrix[i][j] + '0');
+                flip += (char)(!matrix[i][j] + '0');
+            }
+            hash[current]++;
+            hash[flip]++;
+        }
+        int result = 0;
+        
+        for( auto it: hash ){
+            result = max( result, it.second);
+        }
+        return result;
+    }   
+};
+
+//Using 1 string and unordered_map
+class Solution {
+public:
+    int maxEqualRowsAfterFlips(vector<vector<int>>& matrix) {
+        unordered_map<string, int> hash;
+        for( int i = 0 ; i < matrix.size(); ++i ){
+            string current = "";
+            for( int j = 0 ; j < matrix[i].size(); ++j ){
+                if( matrix[i][j] == matrix[i][0] ) current += '1';
+                else current += '0';
+            }
+            hash[current]++;
         }
 
         int result = 0;
