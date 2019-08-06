4c65fd5ac3725f89fcdddf61a1ecd13c987d8cee
jariasf <jariasf03@gmail.com>
2019-08-05 23:58:01 -0500

    Improved 942 LeetCode solution and Added 1700 LintCode solution

---
+++ LintCode/1700 - DI String Match.cpp
@@ -0,0 +1,25 @@
+/*******************************************
+ ***Problema: DI String Match
+ ***ID: 1700
+ ***Juez: LintCode
+ ***Tipo: Ad hoc, Two Pointers
+ ***Autor: Jhosimar George Arias Figueroa
+ *******************************************/
+
+class Solution {
+public:
+    vector<int> diStringMatch(string S) {
+        int n = S.size();
+        vector<int> seq(n + 1);
+        int largest = n, smallest = 0;
+        for( int i = 0 ; i < n ; ++i ){
+            if( S[i] == 'I' ){
+                seq[i] = smallest++;
+            }else{
+                seq[i] = largest--;
+            }
+        }
+        seq[n] = smallest;
+        return seq;
+    }
+};
