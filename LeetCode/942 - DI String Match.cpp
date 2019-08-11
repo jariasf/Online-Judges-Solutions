6097f5954be74ed1f461d2a5b44e1984692a2161
jariasf <jariasf03@gmail.com>
2019-08-10 23:18:30 -0500

    Improved 942 LeetCode solution

--- LeetCode/942 - DI String Match.cpp
+++ LeetCode/942 - DI String Match.cpp
@@ -2,31 +2,24 @@
  ***Problema: DI String Match
  ***ID: 942
  ***Juez: LeetCode
- ***Tipo: Ad hoc, Stack
+ ***Tipo: Ad hoc, Two Pointers
  ***Autor: Jhosimar George Arias Figueroa
  *******************************************/
 
 class Solution {
 public:
-    vector<int> diStringMatch(string s) {
-        int n = s.size();
-        stack<int> S;
-        vector<int> res(n + 1);
-        int j = 0;
+    vector<int> diStringMatch(string S) {
+        int n = S.size();
+        vector<int> seq(n + 1);
+        int largest = n, smallest = 0;
         for( int i = 0 ; i < n ; ++i ){
-            S.push(i);
-            if( s[i] == 'I' ){
-                while( !S.empty() ){
-                    res[j++] = S.top();
-                    S.pop();
-                }
+            if( S[i] == 'I' ){
+                seq[i] = smallest++;
+            }else{
+                seq[i] = largest--;
             }
         }
-        S.push(n);
-        while( !S.empty() ){
-            res[j++] = S.top();
-            S.pop();
-        }
-        return res;
+        seq[n] = smallest;
+        return seq;
     }
 };
