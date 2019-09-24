14888467e980a6a0496731b14bac944843219e6f
jariasf <jariasf03@gmail.com>
2019-09-24 11:51:22 -0500

    Added Shortest Bridge Solution

--- LintCode/1708 - Shortest Bridge.cpp
+++ LintCode/1708 - Shortest Bridge.cpp
@@ -1,15 +1,17 @@
 /*******************************************
  ***Problema: Shortest Bridge
- ***ID: 934
- ***Juez: LeetCode
- ***Tipo: DFS, Flood-fill
+ ***ID: 1708
+ ***Juez: LintCode
+ ***Tipo: DFS | BFS
  ***Autor: Jhosimar George Arias Figueroa
  *******************************************/
 
+// Solution using DFS + BFS on distances
 class Solution {
 public:
+    
     struct Position{
-        int x, y;
+        int x, y, comp;
         Position(int _x, int _y):x(_x), y(_y){}
         Position(){}
     };
@@ -43,7 +45,6 @@ public:
             for( int i = 0 ; i < 4 ; ++i ){
                 int nx = x + dx[i], ny = y + dy[i];
                 if( nx >= 0 && ny >= 0 && nx < h && ny < w){
-
                     if(A[nx][ny] == 0){
                         A[nx][ny] = A[x][y] + 1;
                         Q.push(Position(nx, ny));
@@ -56,7 +57,7 @@ public:
         return -1;
     }
     
-    int shortestBridge(vector<vector<int>>& A) {
+    int ShortestBridge(vector<vector<int>> &A) {
         int h = A.size(), w = A[0].size();
         queue<Position> Q;
         int comp = COMP;
@@ -67,6 +68,67 @@ public:
                 }
             }
         }
-        return bfs(A, Q);     
+        return bfs(A, Q);
+    }
+};
+
+
+
+
+// Solution using DFS + Brute Force on distances
+class Solution {
+public:
+    #define pii pair<int,int>
+    #define mp make_pair
+    vector<pii> border[2];    
+    
+    int dx[4] = {0, 0, 1, -1};
+    int dy[4] = {1, -1, 0, 0};
+    
+    void floodfill(vector<vector<int>> &A, int x, int y, int comp){
+        int h = A.size(), w = A[0].size();
+        A[x][y] = -1;
+        
+        bool zero = false;
+        for( int i = 0 ; i < 4 ; ++i ){
+            int nx = x + dx[i], ny = y + dy[i];
+            if( nx >= 0 && ny >= 0 && nx < h && ny < w ){
+                if( A[nx][ny] == 0) zero = true;
+                else if( A[nx][ny] == 1 )
+                    floodfill(A, nx, ny, comp);
+            }
+        }
+        
+        if( zero ){
+            border[comp].push_back( mp(x,y));
+        }
+    }
+    
+    int dist(pii p1, pii p2){
+        return abs( p1.first - p2.first) + abs(p1.second - p2.second);
+    }
+    
+    int ShortestBridge(vector<vector<int>> &A) {
+        int h = A.size(), w = A[0].size();
+         
+        border[0].clear();
+        border[1].clear();         
+
+        int comp = 0;
+        for( int i = 0 ; i < h ; ++i ){
+            for( int j = 0; j < w ; ++j ){
+                if( A[i][j] == 1 ){
+                    floodfill(A, i, j, comp++);
+                }
+            }
+        }
+        int smallest = 1<<30;
+        for( int i = 0 ; i < border[0].size() ; ++i ){
+            for( int j = 0 ; j < border[1].size(); ++j  ){
+                smallest = min( smallest, dist(border[0][i], border[1][j]) - 1 );
+            }
+        }
+        
+        return smallest;
     }
 };
