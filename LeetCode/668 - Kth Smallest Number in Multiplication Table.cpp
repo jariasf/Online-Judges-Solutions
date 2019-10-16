50c9e35afcfa44c63e60c1d446ca2bf0a23cb5dc
jariasf <jariasf03@gmail.com>
2019-10-16 00:39:27 -0500

    Added LeetCode 668 and LintCode 1097 SOlutions

---
+++ LeetCode/668 - Kth Smallest Number in Multiplication Table.cpp
@@ -0,0 +1,31 @@
+/*******************************************
+ ***Problema: Kth Smallest Number in Multiplication Table
+ ***ID: 668
+ ***Juez: LeetCode
+ ***Tipo: Binary Search
+ ***Autor: Jhosimar George Arias Figueroa
+ *******************************************/
+
+class Solution {
+public:
+    int countSmallest(int x, int m, int n){
+        int cnt = 0;
+        for( int i = 1 ; i <= n ; ++i )
+            cnt += min(m, x/i);
+        return cnt;
+    } 
+     
+    int findKthNumber(int m, int n, int k) {
+        int left = 1, right = n * m, mid;
+        while( left <= right ){
+            mid = (left + right)/2;
+            int cnt = countSmallest(mid, m, n);
+            if( cnt < k ){
+                left = mid + 1;
+            }else{
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+};
