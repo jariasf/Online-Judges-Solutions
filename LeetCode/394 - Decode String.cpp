28b741e1a6f1b835bc02996e53334f2b7887c45e
jariasf <jariasf03@gmail.com>
2020-02-02 14:03:49 -0500

    Added Leetcode 394 solution

---
+++ LeetCode/394 - Decode String.cpp
@@ -0,0 +1,35 @@
+/*******************************************
+ ***Problema: Decode String
+ ***ID: 394
+ ***Juez: LeetCode
+ ***Tipo: Stack
+ ***Autor: Jhosimar George Arias Figueroa
+ *******************************************/
+
+class Solution {
+public:
+    string decodeString(string s) {
+        int n = s.size();
+        stack<pair<int, int> > S;
+        int cnt = 0;
+        string res = "";
+        for( int i = 0 ; i < n ; ++i ){
+            if( s[i] == '['){
+                S.push(make_pair(i, cnt));
+                cnt = 0;
+            }else if( s[i] == ']'){
+                auto& p = S.top();
+                p.second--;
+                if( p.second == 0 ) 
+                    S.pop();
+                else
+                    i = p.first;
+            }else if( isdigit(s[i]) ){
+                cnt = cnt * 10 + s[i] - '0';            
+            }else{
+                res += s[i];
+            }
+        }
+        return res;
+    }
+};
