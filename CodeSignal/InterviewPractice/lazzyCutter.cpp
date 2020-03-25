6883ecb5c22e3c1ffd7a2bc9b862f5e499af2cea
jariasf <jariasf03@gmail.com>
2020-03-24 23:28:12 -0500

    Added Codesignal -lazzyCutter solution

---
+++ CodeSignal/InterviewPractice/lazzyCutter.cpp
@@ -0,0 +1,11 @@
+/*******************************************
+ ***Problem: Lazy Cutter
+ ***Judge: CodeSignal
+ ***Type: Math
+ ***Author: Jhosimar George Arias Figueroa
+ *******************************************/
+
+int lazyCutter(int n) {
+    return n*(n + 1)/2 + 1;
+}
+
