482f4cba0e45078a2f9ca5a420f0c11491162638
jariasf <jariasf03@gmail.com>
2020-04-13 21:10:17 -0500

    Added LeetCode week 2.6 solution

---
+++ LeetCode/30-Day Challenge/Week 2.6 Contiguous Array.cpp
@@ -0,0 +1,26 @@
+/*******************************************
+ ***Problema: Last Stone Weight
+ ***ID: Week2.5
+ ***Juez: LeetCode
+ ***Tipo: Hashing
+ ***Autor: Jhosimar George Arias Figueroa
+ *******************************************/
+
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int, int> hash;
+        int sum = 0, res = 0;
+        hash[0] = 0;
+        for( int i = 1 ; i <= n ; ++i ){
+            if( nums[i - 1] == 0 ) sum--;
+            else sum++;
+            if( hash.find(sum) != hash.end() ){
+                res = max(res, i - hash[sum]);
+            }else
+                hash[sum] = i;
+        }
+        return res;
+    }
+};
