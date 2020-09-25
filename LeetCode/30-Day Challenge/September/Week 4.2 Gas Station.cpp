c3082f388dcf7457b8d6c0bd76181542166924ea
jariasf <jariasf03@gmail.com>
2020-09-25 00:21:30 -0500

    Added Leetcode Week4.2-September solution

---
+++ LeetCode/30-Day Challenge/September/Week 4.2 Gas Station.cpp
@@ -0,0 +1,25 @@
+/*******************************************
+ ***Problema: Gas Station
+ ***ID: Week4.2
+ ***Juez: LeetCode
+ ***Tipo: Ad hoc
+ ***Autor: Jhosimar George Arias Figueroa
+ *******************************************/
+
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int sz = gas.size();
+        int sum = 0, index = 0, tot = 0;
+        for( int i = 0 ; i < sz ; ++i ){
+            sum += gas[i] - cost[i];
+            tot += gas[i] - cost[i];
+            if( sum < 0 ){
+            	index = i + 1;
+                sum = 0;
+            }
+        }
+        if( tot < 0 ) return -1;
+        return index;
+    }
+};
