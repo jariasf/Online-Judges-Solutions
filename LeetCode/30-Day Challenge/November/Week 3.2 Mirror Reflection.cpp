431bf063cf44e17e1cc9cfa31be7fef71737dc47
jariasf <jariasf03@gmail.com>
2020-11-17 23:52:00 -0500

    Added Leetcode Week3.2-November solutions

---
+++ LeetCode/30-Day Challenge/November/Week 3.2 Mirror Reflection.cpp
@@ -0,0 +1,43 @@
+/*******************************************
+ ***Problema: Mirror Reflection
+ ***ID: Week3.2
+ ***Juez: LeetCode
+ ***Tipo: Least Common Multiple (LCM)
+ ***Autor: Jhosimar George Arias Figueroa
+ *******************************************/
+
+class Solution {
+public:
+    int GCD(int p, int q){
+        if( q == 0 ){
+            return p;
+        }
+        return GCD(q, p % q);
+    } 
+     
+    int LCM(int p, int q){
+        return (p * q) / GCD(p,q);
+    } 
+     
+    int mirrorReflection(int p, int q) {
+        int lcm = LCM(p, q);
+        
+        int k = lcm/p;
+        
+        int r = lcm/q;
+        
+        if( k & 1 ){
+            if( r & 1 ){
+                return 1;
+            }else{
+                return 2;
+            }
+        }else{
+            if( r & 1 ){
+                return 0;
+            }else{
+                return -1;
+            }
+        }
+    }
+};
