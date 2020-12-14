6dfe572c5494d89997699e7133ca2bc6e49e9016
jariasf <jariasf03@gmail.com>
2020-12-13 23:43:37 -0500

    Added Leetcode Week-2.1 December solutions

---
+++ LeetCode/30-Day Challenge/December/Week 2.1 Pairs of Songs With Total Durations Divisible by 60.cpp
@@ -0,0 +1,26 @@
+/*******************************************
+ ***Problema: Pairs of Songs With Total Durations Divisible by 60
+ ***ID: Week2.1
+ ***Juez: LeetCode
+ ***Tipo: Modular Arithmetic + Hashing
+ ***Autor: Jhosimar George Arias Figueroa
+ *******************************************/
+
+class Solution {
+public:
+    int numPairsDivisibleBy60(vector<int>& time) {
+        int n = time.size();
+        for( int i = 0 ; i < n ; ++i ) time[i] %= 60;
+        
+        int result = 0;
+        unordered_map<int, int> hash;
+        for( int i = n - 1 ; i >= 0 ; --i ){
+            int index = (60 - time[i]) % 60;
+            if( hash.find( index ) != hash.end() ){
+                result += hash[index];
+            }
+            hash[time[i]]++;
+        }
+        return result;
+    }
+};
