2d47a35a39193c1d2ad03e926f4c9d266e7bb6ec
jariasf <jariasf03@gmail.com>
2021-02-02 23:49:35 -0500

    Added Leetcode Week-1.2 February solution

---
+++ LeetCode/30-Day Challenge/2021/February/Week 1.2 Trim a Binary Search Tree.cpp
@@ -0,0 +1,30 @@
+/*******************************************
+ ***Problema: Trim a Binary Search Tree
+ ***ID: Week1.2
+ ***Juez: LeetCode
+ ***Tipo: BST Traversal
+ ***Autor: Jhosimar George Arias Figueroa
+ *******************************************/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* trimBST(TreeNode* root, int low, int high) {
+        if( root == NULL ) return NULL;
+        if( root -> val < low ) return trimBST(root -> right, low, high);
+        if( root -> val > high ) return trimBST(root -> left, low, high);
+        root -> left = trimBST(root -> left, low, high);
+        root -> right = trimBST(root -> right, low, high);
+        return root;
+    }
+};
