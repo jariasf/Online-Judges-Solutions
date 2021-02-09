7993518fa580627e5f69dc8acea45740169cc852
jariasf <jariasf03@gmail.com>
2021-02-08 23:09:18 -0500

    Added Leetcode Week-1.6 February solution

---
+++ LeetCode/30-Day Challenge/2021/February/Week 1.6 Shortest Distance to a Character.cpp
@@ -0,0 +1,26 @@
+/*******************************************
+ ***Problema: Shortest Distance to a Character
+ ***ID: Week1.6
+ ***Juez: LeetCode
+ ***Tipo: Ad hoc
+ ***Autor: Jhosimar George Arias Figueroa
+ *******************************************/
+
+class Solution {
+public:
+    vector<int> shortestToChar(string s, char c) {
+        int n = s.size(), pos = -n;
+        vector<int> res(n);        
+        for( int i = 0 ; i < n ; ++i ){
+            if( s[i] == c )
+                pos = i;
+            res[i] = i - pos;
+        }
+        for( int i = n - 1 ; i >= 0 ; --i ){
+            if( s[i] == c )
+                pos = i;
+            res[i] = min(res[i], abs(pos - i));
+        }
+        return res;
+    }
+};
