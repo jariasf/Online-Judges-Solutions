ac663971387d45b3b48419cba7f8dc756086d7c6
jariasf <jariasf03@gmail.com>
2021-02-17 23:34:22 -0500

    Added Leetcode Week-3.1 February solution

---
+++ LeetCode/30-Day Challenge/2021/February/Week 3.1 The K Weakest Rows in a Matrix.cpp
@@ -0,0 +1,33 @@
+/*******************************************
+ ***Problema: The K Weakest Rows in a Matrix
+ ***ID: Week3.1
+ ***Juez: LeetCode
+ ***Tipo: Sorting
+ ***Autor: Jhosimar George Arias Figueroa
+ *******************************************/
+
+class Solution {
+public:
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+        int h = mat.size(), w = mat[0].size();
+        vector<pair<int,int> > row;
+        for( int i = 0 ; i < h ; ++i ){
+            int index = 0;
+            for( int j = 0 ; j < w ; ++j ){
+                if( mat[i][j] == 1 ){
+                    index = j + 1;
+                }else
+                    break;
+            }
+            row.push_back(make_pair(index, i));
+        }
+        
+        sort(row.begin(), row.end());
+        
+        vector<int> res(k);
+        for( int i = 0 ; i < k ; ++i ){
+            res[i] = row[i].second;
+        }
+        return res;
+    }
+};
