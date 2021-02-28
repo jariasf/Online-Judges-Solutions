8edf0a5ee813dfef14db6920f338dc59c918951d
jariasf <jariasf03@gmail.com>
2021-02-27 23:04:58 -0500

    Added Leetcode Week-4.3 February solution

---
+++ LeetCode/30-Day Challenge/2021/February/Week 4.3 Score of Parentheses.cpp
@@ -0,0 +1,35 @@
+/*******************************************
+ ***Problema: Score of Parentheses
+ ***ID: Week4.3
+ ***Juez: LeetCode
+ ***Tipo: Stack
+ ***Autor: Jhosimar George Arias Figueroa
+ *******************************************/
+
+class Solution {
+public:
+    #define pii pair<int,int>
+    #define mp make_pair
+    int scoreOfParentheses(string s) {
+        int result = 0;
+        vector<int> S;
+        for( int i = 0 ; i < s.size() ; ++i ){
+            if( s[i] == '('){
+                S.push_back(0);
+            }else{
+                if( S.size() > 0 ){
+                    int cur = S[S.size() - 1];
+                    S.pop_back();
+                    int value = 2 * cur;
+                    if( cur == 0 )
+                        value = 1;
+                    
+                    if( S.size() > 0 ){
+                        S[ S.size() - 1 ] += value;
+                    }else if( S.size() == 0 ) result += value;
+                }
+            }
+        }
+        return result;
+    }
+};
