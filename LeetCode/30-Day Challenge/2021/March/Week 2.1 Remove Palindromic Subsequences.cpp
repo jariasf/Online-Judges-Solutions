577b5f2d4574a1f0bd8d25c6c4bd99432927d99d
jariasf <jariasf03@gmail.com>
2021-03-13 23:45:29 -0500

    Added Leetcode Week-2.1 March solution

---
+++ LeetCode/30-Day Challenge/2021/March/Week 2.1 Remove Palindromic Subsequences.cpp
@@ -0,0 +1,18 @@
+/*******************************************
+ ***Problema: Remove Palindromic Subsequences
+ ***ID: Week2.1
+ ***Juez: LeetCode
+ ***Tipo: Ad hoc
+ ***Autor: Jhosimar George Arias Figueroa
+ *******************************************/
+
+class Solution {
+public:
+    int removePalindromeSub(string s) {
+        if( s.size() == 0 ) return 0;
+        string T = s;
+        reverse(T.begin(), T.end());
+        if( T == s ) return 1;
+        return 2;
+    }
+};
