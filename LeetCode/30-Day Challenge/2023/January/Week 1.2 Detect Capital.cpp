8f59f05176b091e602d6ad3ff8c871b6b64c3e04
jariasf <jariasf03@gmail.com>
2023-01-02 00:34:12 -0500

    Added leetcode january week1.2-2023 solution

---
+++ LeetCode/30-Day Challenge/2023/January/Week 1.2 Detect Capital.cpp
@@ -0,0 +1,22 @@
+﻿/*******************************************
+ ***Problema: Detect Capital
+ ***ID: Week 1.2
+ ***Juez: LeetCode
+ ***Tipo: Ad hoc
+ ***Autor: Jhosimar George Arias Figueroa
+ *******************************************/
+
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int cnt_upper{0}, upper_index{-1};
+        for(int i = 0; i < word.size(); ++i){
+            if(isupper(word[i])){
+                cnt_upper++;
+                upper_index = i;
+            }
+        }
+        return (cnt_upper == word.size()) ||
+               (cnt_upper == 0 || cnt_upper == 1 && upper_index == 0); 
+    }
+};
