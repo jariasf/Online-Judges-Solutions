5a1ba594bf6424ed315d540a15f36051dd7cae7d
jariasf <jariasf03@gmail.com>
2023-01-01 22:13:43 -0500

    Added leetcode january week1.1-2023 solution

---
+++ LeetCode/30-Day Challenge/2023/January/Week 1.1 Word Pattern.cpp
@@ -0,0 +1,29 @@
+﻿/*******************************************
+ ***Problema: Word Pattern
+ ***ID: Week 1.1
+ ***Juez: LeetCode
+ ***Tipo: Hashing
+ ***Autor: Jhosimar George Arias Figueroa
+ *******************************************/
+
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        unordered_map<char,string> mapping;
+        unordered_map<string, char> mapping2;
+        stringstream ss(s);
+        for(char c:pattern){
+            if(ss>>s){
+                if( mapping.count(c) ){
+                    if(mapping[c] != s) return false;
+                }else{
+                    if(mapping2.count(s)) return false;
+                    mapping2[s] = c;
+                    mapping[c] = s;
+                }
+            }else
+                return false;
+        }
+        return !(ss>>s);
+    }
+};
