504460690e2401e09f808ae31bedec03e66363a3
jariasf <jariasf03@gmail.com>
2023-01-02 20:31:51 -0500

    Added leetcode january week1.3-2023 solution

---
+++ LeetCode/30-Day Challenge/2023/January/Week 1.3 Delete Columns to Make Sorted.cpp
@@ -0,0 +1,25 @@
+﻿/*******************************************
+ ***Problema: Delete Columns to Make Sorted
+ ***ID: Week 1.3
+ ***Juez: LeetCode
+ ***Tipo: Ad hoc
+ ***Autor: Jhosimar George Arias Figueroa
+ *******************************************/
+
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        int n = strs.size(), m = strs[0].size();
+        int res = 0;
+        for(int j = 0; j < m ; ++j){
+            for(int i = 1 ; i < n ; ++i){
+                if( strs[i][j] < strs[i - 1][j]){
+                    res++;
+                    break;
+                }
+            }
+            
+        }
+        return res;
+    }
+};
