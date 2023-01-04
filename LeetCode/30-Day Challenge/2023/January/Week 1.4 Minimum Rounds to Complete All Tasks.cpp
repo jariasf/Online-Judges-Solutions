da293ef6557d4f9b19e200721d52f18d90b931ab
jariasf <jariasf03@gmail.com>
2023-01-03 20:13:17 -0500

    Added leetcode january week1.4-2023 solution

---
+++ LeetCode/30-Day Challenge/2023/January/Week 1.4 Minimum Rounds to Complete All Tasks.cpp
@@ -0,0 +1,25 @@
+﻿/*******************************************
+ ***Problema: Minimum Rounds to Complete All Tasks
+ ***ID: Week 1.4
+ ***Juez: LeetCode
+ ***Tipo: Hashing - frequencies
+ ***Autor: Jhosimar George Arias Figueroa
+ *******************************************/
+
+class Solution {
+public:
+    int minimumRounds(vector<int>& tasks) {
+        unordered_map<int, int> hash;
+        for(int num:tasks){
+            hash[num]++;
+        }
+        int res = 0;
+        for(auto it:hash){
+            int cnt = it.second;
+            if( cnt == 1 ) return -1;
+            else 
+                res += cnt/3 + (cnt % 3 > 0);
+        }
+        return res;
+    }
+};
