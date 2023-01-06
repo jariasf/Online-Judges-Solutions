86fe6e1c30e4501aa0fb5172676a591ee0a0880b
jariasf <jariasf03@gmail.com>
2023-01-05 23:47:16 -0500

    Added leetcode january week1.6-2023 solution

---
+++ LeetCode/30-Day Challenge/2023/January/Week 1.6 Maximum Ice Cream Bars.cpp
@@ -0,0 +1,42 @@
+﻿/*******************************************
+ ***Problema: Maximum Ice Cream Bars
+ ***ID: Week 1.6
+ ***Juez: LeetCode
+ ***Tipo: Greedy – Sorting | Hashing
+ ***Autor: Jhosimar George Arias Figueroa
+ *******************************************/
+
+// Sorting
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        sort(costs.begin(), costs.end());
+        int res = 0;
+        for(int v: costs){
+            if( coins < v ) break;
+            coins -=v;
+            res++;
+        }
+        return res;
+    }
+};
+
+
+// Hashing
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        int res = 0;
+        int maxi = *max_element(costs.begin(), costs.end());
+        int mini = *min_element(costs.begin(), costs.end());
+        vector<int> hash(maxi + 1);
+        for(int v:costs) hash[v]++;
+        for(int cost = mini ; cost <= maxi ; ++cost){
+            if(hash[cost] == 0) continue;
+            int min_coins = min(coins/hash[cost], hash[cost]);
+            hash[cost] -= min_coins;
+            res += min_coins;
+        }
+        return res;
+    }
+};
