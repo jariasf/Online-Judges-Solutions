b5369ee0b3d49f0929494f0c3431930a96ffd416
jariasf <jariasf03@gmail.com>
2023-01-06 23:56:17 -0500

    Added leetcode january week1.7-2023 solution

---
+++ LeetCode/30-Day Challenge/2023/January/Week 1.7 Gas Station.cpp
@@ -0,0 +1,25 @@
+﻿/*******************************************
+ ***Problema: Gas Station
+ ***ID: Week 1.7
+ ***Juez: LeetCode
+ ***Tipo: Greedy
+ ***Autor: Jhosimar George Arias Figueroa
+ *******************************************/
+
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int sz = gas.size();
+        int sum = 0, index = 0, tot = 0;
+        for( int i = 0 ; i < sz ; ++i ){
+            sum += gas[i] - cost[i];
+            tot += gas[i] - cost[i];
+            if( sum < 0 ){
+                index = i + 1;
+                sum = 0;
+            }
+        }
+        if( tot < 0 ) return -1;
+        return index;
+    }
+};
