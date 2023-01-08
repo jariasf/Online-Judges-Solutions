1c338a8cf735add024a1b2b5955441d2421a2c97
jariasf <jariasf03@gmail.com>
2023-01-08 00:41:28 -0500

    Added leetcode january week2.1-2023 solution

---
+++ LeetCode/30-Day Challenge/2023/January/Week 2.1 Max Points on a Line.cpp
@@ -0,0 +1,35 @@
+﻿/*******************************************
+ ***Problema: Max Points on a Line
+ ***ID: Week 2.1
+ ***Juez: LeetCode
+ ***Tipo: Hashing | Map + Math
+ ***Autor: Jhosimar George Arias Figueroa
+ *******************************************/
+
+class Solution {
+public:
+    #define pii pair<double,double>
+    #define mp make_pair
+    int maxPoints(vector<vector<int>>& points) {
+        int n = points.size();        
+        int maxi = 1;
+        double dx, dy, slope, intercept;
+        map<pii, int> hash;
+        for(int i = 0 ; i < n ; ++i){
+            hash.clear();
+            for(int j = 0 ; j < n ; ++j){
+                if( i == j ) continue;
+                dx = points[j][0] - points[i][0];
+                dy = points[j][1] - points[i][1];
+                slope = INT_MAX, intercept = points[i][0];
+                if(dx != 0){
+                    slope = dy/dx;
+                    intercept = points[i][1] - slope * points[i][0];
+                }
+                hash[mp(slope, intercept)]++;
+                maxi = max(maxi, hash[mp(slope, intercept)] + 1);
+            }
+        }
+        return maxi;
+    }
+};
