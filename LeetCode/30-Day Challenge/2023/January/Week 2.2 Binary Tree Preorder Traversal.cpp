bf005753e7c0a25b89e6f52ab21ee2898e3d8095
jariasf <jariasf03@gmail.com>
2023-01-08 23:27:54 -0500

    Added leetcode january week2.2-2023 solution

---
+++ LeetCode/30-Day Challenge/2023/January/Week 2.2 Binary Tree Preorder Traversal.cpp
@@ -0,0 +1,35 @@
+﻿/*******************************************
+ ***Problema: Binary Tree Preorder Traversal
+ ***ID: Week 2.2
+ ***Juez: LeetCode
+ ***Tipo: Stack
+ ***Autor: Jhosimar George Arias Figueroa
+ *******************************************/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        if( root == nullptr ) return {};
+        vector<int> traversal;
+        stack<TreeNode*> S;
+        S.push(root);
+        while(!S.empty()){
+            TreeNode* cur = S.top(); S.pop();
+            traversal.push_back(cur->val);
+            if(cur -> right) S.push(cur -> right);
+            if(cur -> left) S.push(cur -> left);
+        }
+        return traversal;
+    }
+};
