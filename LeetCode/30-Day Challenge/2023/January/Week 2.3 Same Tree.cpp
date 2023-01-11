d25623b8cfc28ee44dca07116c91ff048942c12d
jariasf <jariasf03@gmail.com>
2023-01-10 23:53:15 -0500

    Added leetcode january week2.4-2023 solution

---
+++ LeetCode/30-Day Challenge/2023/January/Week 2.3 Same Tree.cpp
@@ -0,0 +1,60 @@
+﻿/*******************************************
+ ***Problema: Same Tree
+ ***ID: Week 2.3
+ ***Juez: LeetCode
+ ***Tipo: Recurion | BFS
+ ***Autor: Jhosimar George Arias Figueroa
+ *******************************************/
+
+// Recursion
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if((!p && q) || (p && !q) ) return false;
+        if(!p && !q) return true;
+        if(p-> val != q -> val) return false;
+        return isSameTree(p -> left, q -> left) && isSameTree(p -> right, q -> right);
+    }
+};
+
+
+// Iterative
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        queue<TreeNode*> Q1, Q2;
+        Q1.push(p); Q2.push(q);
+        while(!Q1.empty() && !Q2.empty()){
+            p = Q1.front(); Q1.pop();
+            q = Q2.front(); Q2.pop();
+            if((!p && q) || (p && !q) ) return false;
+            if(!p && !q) continue;
+            if(p-> val != q -> val) return false;
+            Q1.push(p->left); Q2.push(q->left);
+            Q1.push(p->right); Q2.push(q->right);
+        }
+        return true;
+    }
+};
---
+++ LeetCode/30-Day Challenge/2023/January/Week 2.4 Minimum Time to Collect All Apples in a Tree.cpp
@@ -0,0 +1,36 @@
+﻿/*******************************************
+ ***Problema: Minimum Time to Collect All Apples in a Tree
+ ***ID: Week 2.4
+ ***Juez: LeetCode
+ ***Tipo: DFS | BFS
+ ***Autor: Jhosimar George Arias Figueroa
+ *******************************************/
+
+class Solution {
+public:
+    vector<vector<int>> adj;
+    vector<bool> has_apple;
+    int dfs(int u, int parent){
+        int tot = 0;
+        for(int v: adj[u]){
+            if(v != parent){                
+                int child_has_apple = dfs(v, u);
+                if( child_has_apple || has_apple[v])
+                    tot += child_has_apple + 2;
+            }
+        }
+        return tot;
+    }
+
+    int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) {
+        adj = vector<vector<int> >(n+1);
+        has_apple = hasApple;
+        int E = edges.size();
+        for(int i = 0 ; i < E; ++i){
+            int u = edges[i][0], v = edges[i][1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        return dfs(0, -1);
+    }
+};
