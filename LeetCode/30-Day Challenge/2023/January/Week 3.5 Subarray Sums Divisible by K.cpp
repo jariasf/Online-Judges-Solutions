d6903f7deb4dd93da18323451948f6bd090be25f
jariasf <jariasf03@gmail.com>
2023-01-18 23:42:55 -0500

    Added leetcode january week3.5-2023 solution

---
+++ LeetCode/30-Day Challenge/2023/January/Week 3.5 Subarray Sums Divisible by K.cpp
@@ -0,0 +1,24 @@
+﻿/*******************************************
+ ***Problema: Subarray Sums Divisible by K
+ ***ID: Week 3.5
+ ***Juez: LeetCode
+ ***Tipo: Prefix sums
+ ***Autor: Jhosimar George Arias Figueroa
+ *******************************************/
+
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> sums(k, 0);
+        sums[0]++;
+        int cnt = 0;
+        int currSum = 0;
+        for(int i = 0; i<n; i++) {
+            currSum = (currSum + nums[i]%k + k)%k;
+            cnt += sums[currSum];
+            sums[currSum]++;
+        }
+        return cnt;
+    }
+};
