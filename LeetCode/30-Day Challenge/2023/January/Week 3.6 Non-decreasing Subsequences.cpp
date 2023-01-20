de3fa73fea4c78569ce55263935bce62d4f4ced2
jariasf <jariasf03@gmail.com>
2023-01-19 23:55:51 -0500

    Added leetcode january week3.6-2023 solution

---
+++ LeetCode/30-Day Challenge/2023/January/Week 3.6 Non-decreasing Subsequences.cpp
@@ -0,0 +1,38 @@
+﻿/*******************************************
+ ***Problema: Non-decreasing Subsequences
+ ***ID: Week 3.6
+ ***Juez: LeetCode
+ ***Tipo: Backtracking
+ ***Autor: Jhosimar George Arias Figueroa
+ *******************************************/
+
+class Solution {
+public:
+
+    void solve(vector<vector<int> > &res, vector<int> current, int pos, vector<int> &v){
+        int n = v.size();
+        if( pos == n ){
+            if(current.size() > 1)
+                res.push_back(current);
+            return;
+        }
+
+        if( current.empty() || v[pos] != current.back() ){
+            solve(res, current, pos + 1, v);
+        }
+
+        if( current.empty() || v[pos] >= current.back() ){
+            current.push_back(v[pos]);
+            solve(res, current, pos + 1, v);
+            current.pop_back();
+        }
+    }
+
+
+    vector<vector<int>> findSubsequences(vector<int>& nums) {
+        vector<vector<int> > res;
+        vector<int> cur;
+        solve(res, cur, 0, nums);
+        return res;
+    }
+};
