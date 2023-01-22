cf5c53b56cc651b150c6654a6f21e9233880ae3b
jariasf <jariasf03@gmail.com>
2023-01-22 00:54:43 -0500

    Added leetcode january week4.1-2023 solution

---
+++ LeetCode/30-Day Challenge/2023/January/Week 4.1 Palindrome Partitioning.cpp
@@ -0,0 +1,46 @@
+﻿/*******************************************
+ ***Problema: Palindrome Partitioning
+ ***ID: Week 4.1
+ ***Juez: LeetCode
+ ***Tipo: Backtracking
+ ***Autor: Jhosimar George Arias Figueroa
+ *******************************************/
+
+class Solution {
+public:
+
+    bool isPalindrome(string &s, int left, int right){
+        while(left < right){
+            if( s[left] != s[right - 1]) return false;
+            left++; right--;
+        }
+        return true;
+    }
+
+    void backtrack(vector<vector<string>> &res, vector<string> current, string &s, int pos ){
+        int n = s.size();
+        if( pos == n ){
+            res.push_back(current);
+            return;
+        }
+
+        string pal = "";
+        for(int i = pos ; i < n ; ++i ){
+            pal += s[i];
+            if( isPalindrome(s, pos, i + 1)){
+                //current.push_back(s.substr(pos, i - pos + 1));
+                current.push_back(pal);
+                backtrack(res, current, s, i + 1);
+                current.pop_back();
+            }
+        }
+
+    }
+
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> res;
+        vector<string> current;
+        backtrack(res, current, s, 0);
+        return res;
+    }
+};
