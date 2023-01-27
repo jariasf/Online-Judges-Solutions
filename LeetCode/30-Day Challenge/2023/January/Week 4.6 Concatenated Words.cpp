c2b31f8c7bd22619e5912f9b928bd6afbc838607
jariasf <jariasf03@gmail.com>
2023-01-26 23:30:25 -0500

    Added leetcode january week4.6-2023 solution

---
+++ LeetCode/30-Day Challenge/2023/January/Week 4.6 Concatenated Words.cpp
@@ -0,0 +1,40 @@
+﻿/*******************************************
+ ***Problema: Concatenated Words
+ ***ID: Week 4.6
+ ***Juez: LeetCode
+ ***Tipo: Recursion
+ ***Autor: Jhosimar George Arias Figueroa
+ *******************************************/
+
+
+// O(n*l + n*l^3)
+
+class Solution {
+public:
+
+    unordered_set<string> hash;
+
+    bool possible(string &s, int pos, int cnt){
+        int n = s.size();
+        if( pos == n ){
+            return cnt > 1;
+        }
+        string str="";
+        for(int i = pos ; i < n; ++i){
+            str += s[i];
+            if( hash.count(str) && (possible(s, i + 1, cnt + 1)) ) return true;
+        }
+        return false;
+    }
+
+    vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
+        for(string w: words) hash.insert(w);
+        vector<string> res;
+        for(string w:words){
+            if(possible(w, 0, 0)){
+                res.push_back(w);
+            }
+        }
+        return res;
+    }
+};
