4fa2b0c29c21591f74ce450ce544cb83b0eb84f6
jariasf <jariasf03@gmail.com>
2023-01-29 20:42:32 -0500

    Added leetcode january week5.1-2023 solution

---
+++ LeetCode/30-Day Challenge/2023/January/Week 5.1 N-th Tribonacci Number.cpp
@@ -0,0 +1,23 @@
+﻿/*******************************************
+ ***Problema: N-th Tribonacci Number
+ ***ID: Week 5.1
+ ***Juez: LeetCode
+ ***Tipo: Ad hoc
+ ***Autor: Jhosimar George Arias Figueroa
+ *******************************************/
+
+class Solution {
+public:
+    int tribonacci(int n) {
+        if( n == 0 ) return 0;
+        if( n < 3 ) return 1;
+        int a = 0, b = 1, c = 1, d = 2;
+        for(int i = 3 ; i <= n ; ++i){
+            d = a + b + c;
+            a = b;
+            b = c;
+            c = d;
+        }
+        return d;
+    }
+};
