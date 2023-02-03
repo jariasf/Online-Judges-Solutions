50ebc3aa9ad6790fecc8671d26d4205854cd9b0a
jariasf <jariasf03@gmail.com>
2023-02-02 23:08:42 -0500

    Added leetcode february week1.3-2023 solution

---
+++ LeetCode/30-Day Challenge/2023/February/Week 1.3 Zigzag Conversion.cpp
@@ -0,0 +1,29 @@
+﻿/*******************************************
+ ***Problema: Zigzag Conversion
+ ***ID: Week 1.3
+ ***Juez: LeetCode
+ ***Tipo: Ad hoc
+ ***Autor: Jhosimar George Arias Figueroa
+ *******************************************/
+
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if( numRows == 1 ) return s;
+        int n = s.size(), incr = numRows + (numRows - 2);
+        string res = "";
+        for(int i = 0 ; i < numRows; ++i){
+            int j = i;
+            while( j < n ){
+                res += s[j];
+                if( i > 0 && i < numRows - 1){
+                    int k = j + incr - 2 * i; 
+                    if( k < n )
+                        res += s[k];
+                }
+                j += incr;
+            }
+        }
+        return res;
+    }
+};
