18361f04d98616dac09e24bc85be7ee7475689ec
jariasf <jariasf03@gmail.com>
2023-02-03 20:29:03 -0500

    Added leetcode february week1.4-2023 solution

---
+++ LeetCode/30-Day Challenge/2023/February/Week 1.4 Permutation in String.cpp
@@ -0,0 +1,27 @@
+﻿/*******************************************
+ ***Problema: Permutation in String
+ ***ID: Week 1.4
+ ***Juez: LeetCode
+ ***Tipo: Sliding window - hashing
+ ***Autor: Jhosimar George Arias Figueroa
+ *******************************************/
+
+class Solution{
+public:
+    bool checkInclusion(string s1, string s2) {
+        int n = s2.size(), total = s1.size();
+        vector<int> hash(26);
+        for( int i = 0 ; i < total ; ++i ) 
+            hash[s1[i] - 'a']++;
+        for( int i = 0, left = 0; i < n ; ++i ){
+            hash[s2[i] - 'a']--;
+            total--;
+            while( hash[ s2[i] - 'a'] < 0 ){
+                hash[s2[left++] - 'a']++;
+                total++;
+            }
+            if( total == 0 ) return true;
+        }
+        return false;
+    }
+};
