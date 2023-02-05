8acfa9961ba19684a161cd5934943c5e36faa885
jariasf <jariasf03@gmail.com>
2023-02-04 23:14:39 -0500

    Added leetcode february week1.5-2023 solution

---
+++ LeetCode/30-Day Challenge/2023/February/Week 1.5 Find All Anagrams in a String.cpp
@@ -0,0 +1,36 @@
+﻿/*******************************************
+ ***Problema: Find All Anagrams in a String
+ ***ID: Week 1.5
+ ***Juez: LeetCode
+ ***Tipo: Sliding window - hashing
+ ***Autor: Jhosimar George Arias Figueroa
+ *******************************************/
+
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        vector<int> hash(26);
+        
+        for(char c:p){
+            hash[c - 'a']++;
+        }
+        
+        int total = p.size();
+        vector<int> res;
+        
+        for( int i = 0, left = 0 ; i < s.size(); ++i ){
+            hash[s[i] - 'a']--;
+            total--;
+            
+            while( hash[s[i] - 'a'] < 0 ){
+                total++;
+                hash[ s[left++] - 'a']++;
+            }
+
+            if( total == 0 ){
+                res.push_back(left);
+            }
+        }
+        return res;
+    }
+};
