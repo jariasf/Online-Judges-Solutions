0224d0a1f2ae1c704fc2078ae3d1fbf6605e381f
jariasf <jariasf03@gmail.com>
2023-02-05 22:10:56 -0500

    Added leetcode february week1.6-2023 solution

---
+++ LeetCode/30-Day Challenge/2023/February/Week 1.6 Shuffle the Array.cpp
@@ -0,0 +1,19 @@
+﻿/*******************************************
+ ***Problema: Shuffle the Array
+ ***ID: Week 1.6
+ ***Juez: LeetCode
+ ***Tipo: Ad hoc
+ ***Autor: Jhosimar George Arias Figueroa
+ *******************************************/
+
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> res(2*n);
+        for(int i = 0 ; i < n ; ++i){
+            res[2*i] = nums[i];
+            res[2*i + 1] = nums[i + n];
+        }
+        return res;
+    }
+};
