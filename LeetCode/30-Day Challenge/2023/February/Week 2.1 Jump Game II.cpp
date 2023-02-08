bb8aaf7269f850950ccadd6e9e526adfec65cead
jariasf <jariasf03@gmail.com>
2023-02-07 22:47:27 -0500

    Added leetcode february week2.1-2023 solution

---
+++ LeetCode/30-Day Challenge/2023/February/Week 2.1 Jump Game II.cpp
@@ -0,0 +1,23 @@
+﻿/*******************************************
+ ***Problema: Jump Game II
+ ***ID: Week 2.1
+ ***Juez: LeetCode
+ ***Tipo: Greedy
+ ***Autor: Jhosimar George Arias Figueroa
+ *******************************************/
+
+
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size(), steps = 0, next_reach = 0;
+        for( int i = 0, reach = 0 ; i < n - 1 && i <= reach  ; ++i ){
+            next_reach = max(next_reach, nums[i] + i);  
+            if( i == reach ){
+                steps++;
+                reach = next_reach;
+            }
+        }
+        return steps;
+    }
+};
