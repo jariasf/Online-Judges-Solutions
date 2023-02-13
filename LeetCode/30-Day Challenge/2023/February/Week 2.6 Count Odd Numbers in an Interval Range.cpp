46ba4f2e5be29d9b9f2df8490b015e7a63d6cf9d
jariasf <jariasf03@gmail.com>
2023-02-12 20:52:23 -0500

    Added leetcode february week2.6-2023 solution

---
+++ LeetCode/30-Day Challenge/2023/February/Week 2.6 Count Odd Numbers in an Interval Range.cpp
@@ -0,0 +1,16 @@
+﻿/*******************************************
+ ***Problema: Count Odd Numbers in an Interval Range
+ ***ID: Week 2.6
+ ***Juez: LeetCode
+ ***Tipo: Math
+ ***Autor: Jhosimar George Arias Figueroa
+ *******************************************/
+
+class Solution {
+public:
+    int countOdds(int low, int high) {
+        if(!(low & 1)) low++;
+        if(!(high & 1)) high--;
+        return (high - low + 2)/2; 
+    }
+};
