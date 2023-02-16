27e6d5184c5ad2398255410f6edec40185e25e70
jariasf <jariasf03@gmail.com>
2023-02-15 20:09:21 -0500

    Added leetcode february week3.2-2023 solution

---
+++ LeetCode/30-Day Challenge/2023/February/Week 3.2 Maximum Depth of Binary Tree.cpp
@@ -0,0 +1,26 @@
+﻿/*******************************************
+ ***Problema: Maximum Depth of Binary Tree
+ ***ID: Week 3.2
+ ***Juez: LeetCode
+ ***Tipo: Tree traversal
+ ***Autor: Jhosimar George Arias Figueroa
+ *******************************************/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if( root == nullptr) return 0;
+        return 1 + max(maxDepth(root ->left), maxDepth(root -> right));
+    }
+};
