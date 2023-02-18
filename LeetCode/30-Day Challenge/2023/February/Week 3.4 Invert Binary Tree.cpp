17fae8e9282e4f2717a3068b526192f342615ce4
jariasf <jariasf03@gmail.com>
2023-02-17 22:56:07 -0500

    Added leetcode february week3.4-2023 solution

---
+++ LeetCode/30-Day Challenge/2023/February/Week 3.4 Invert Binary Tree.cpp
@@ -0,0 +1,30 @@
+﻿/*******************************************
+ ***Problema: Invert Binary Tree
+ ***ID: Week 3.4
+ ***Juez: LeetCode
+ ***Tipo: Tree traversal
+ ***Autor: Jhosimar George Arias Figueroa
+ *******************************************/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if( root == nullptr ) return nullptr;
+        TreeNode* left = invertTree(root -> left);
+        TreeNode* right = invertTree(root -> right);
+        root -> left = right;
+        root -> right = left;
+        return root;
+    }
+};
