5397c056b82c4cd0d27b30f58daff3c16fbbce8c
jariasf <jariasf03@gmail.com>
2023-02-20 00:23:09 -0500

    Added leetcode february week3.6-2023 solution

---
+++ LeetCode/30-Day Challenge/2023/February/Week 3.6 Search Insert Position.cpp
@@ -0,0 +1,25 @@
+﻿/*******************************************
+ ***Problema: Search Insert Position
+ ***ID: Week 3.6
+ ***Juez: LeetCode
+ ***Tipo: Binary Search
+ ***Autor: Jhosimar George Arias Figueroa
+ *******************************************/
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left = 0 , right = nums.size(), mid;
+        while( left < right ){
+            mid = (left + right) / 2;
+            if( nums[mid] == target ){
+                return mid;
+            }else if( nums[mid] < target){
+                left = mid + 1;
+            }else{
+                right = mid;
+            }
+        }
+        return left;
+    }
+};
