513eb9c8045e4b043d9b0243a624b02579705b1c
jariasf <jariasf03@gmail.com>
2023-02-23 00:19:22 -0500

    Added leetcode february week4.2-2023 solution

---
+++ LeetCode/30-Day Challenge/2023/February/Week 4.2 IPO.cpp
@@ -0,0 +1,33 @@
+﻿/*******************************************
+ ***Problema: IPO
+ ***ID: Week 4.2
+ ***Juez: LeetCode
+ ***Tipo: Priority queue + sorting
+ ***Autor: Jhosimar George Arias Figueroa
+ *******************************************/
+
+class Solution {
+public:
+    #define pii pair<int, int>
+    #define mp make_pair
+
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+        int n = profits.size();
+        vector<pii> v(n);        
+        for(int i = 0 ; i < n ; ++i)
+            v[i] = mp(capital[i], profits[i]);
+        
+        sort(v.begin(), v.end());
+        priority_queue<int> Q;
+        int i = 0;
+        while(k > 0){
+            while(i < n && v[i].first <= w){
+                Q.push(v[i++].second);
+            }
+            if(Q.empty()) break;
+            w += Q.top(); Q.pop();
+            k--;
+        }
+        return w;
+    }
+};
