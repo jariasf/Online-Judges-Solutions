f6131038adb8ec7b61d9d82eaa338af92bd42597
jariasf <jariasf03@gmail.com>
2023-02-24 00:35:07 -0500

    Added leetcode february week4.3-2023 solution

---
+++ LeetCode/30-Day Challenge/2023/February/Week 4.3 Minimize Deviation in Array.cpp
@@ -0,0 +1,34 @@
+﻿/*******************************************
+ ***Problema: Minimize Deviation in Array
+ ***ID: Week 4.3
+ ***Juez: LeetCode
+ ***Tipo: Priority queue
+ ***Autor: Jhosimar George Arias Figueroa
+ *******************************************/
+
+class Solution {
+public:
+    int minimumDeviation(vector<int>& nums) {
+        int n = nums.size();
+        int mini = INT_MAX;
+        priority_queue<int> Q;
+        for(int i = 0 ; i < n ; ++i){
+            while(nums[i] % 2 != 0) nums[i] *= 2;
+            mini = min(mini, nums[i]);
+            Q.push(nums[i]);
+        }
+
+        int res = Q.top() - mini;
+        while(!Q.empty() ){
+            int val = Q.top();
+            res = min(res, val - mini);
+            if( val % 2 != 0 ) break;
+            val /= 2;
+            if( val < mini )
+                mini = val;
+            Q.pop();
+            Q.push(val);
+        }
+        return res;
+    }
+};
