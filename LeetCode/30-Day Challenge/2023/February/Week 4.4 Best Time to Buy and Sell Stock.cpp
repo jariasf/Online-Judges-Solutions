1fea5b8598e45d32c7c13f082ed77149eb0ccf14
jariasf <jariasf03@gmail.com>
2023-02-25 00:14:31 -0500

    Added leetcode february week4.4-2023 solution

---
+++ LeetCode/30-Day Challenge/2023/February/Week 4.4 Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,19 @@
+﻿/*******************************************
+ ***Problema: Best Time to Buy and Sell Stock
+ ***ID: Week 4.4
+ ***Juez: LeetCode
+ ***Tipo: DP
+ ***Autor: Jhosimar George Arias Figueroa
+ *******************************************/
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mini = prices[0], res = 0;
+        for(int price:prices){
+            res = max(res, price - mini);
+            mini = min(mini, price);
+        }
+        return res;
+    }
+};
